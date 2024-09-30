8c7df538de677415d6b349a5b83d3852720a5e2e
Emmanuel Quainoo <equainoo@ivytech.edu>
2024-09-30 16:00:54 -0500

    first commit

---
+++ regproject/regapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from regapp.models import UserProfileInfo
+from regapp import forms
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+
+def registration(request):
+    registered = False
+    
+    if request.method == "POST":
+        userform = forms.UserForm(data=request.POST)
+        profileform = forms.UserProfileInfoForm(data=request.POST)
+
+        if userform.is_valid() and profileform.is_valid():
+            userform.save(commit=True)
+            profileform.save(commit=True)
+            
+            registered = True
+    else:
+        userform = forms.UserForm()
+        profileform = forms.UserProfileInfoForm()
+        
+        
+            
+    return render(request, 'register.html', {'userform': userform, 'profileform': profileform, 'registered': registered})
+
+
+    
